cea514121826878780a5464c05da3a224e899831
Chaldun <chaldunalder@gmail.com>
2021-04-11 16:15:16 -0500

    •Password Recovery scene created •Functional input fields/dropdown implemented -Email,Security Questions 1&2, Answer Fields 1&2 •Validation with all fields -Tests if theres input then correct input & finally if records are found •Submit button works as intended, -If validation is passed and record is found will send a email out to the email requested and if successful label will become visible showing successfully sent •Login button takes you back to the Login_Page.fxml

--- src/sample/DataValidation.java
+++ src/sample/DataValidation.java
@@ -38,4 +38,50 @@ public class DataValidation
             throw new IllegalArgumentException();
         }
     }
+    public static void Password_Recovery_Validator(String Rec_Email, String Q1, String Q1_Answer, String Q2, String Q2_Answer)
+    {
+        String Email_Regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
+        String Answer_Format = "^[a-zA-Z0-9 ._-]{1,25}$";
+        TrayNotification tray = new TrayNotification();
+        AnimationType type = AnimationType.POPUP;
+        tray.setAnimationType(type);
+
+
+        if (!Rec_Email.matches(Email_Regex))
+        {
+            tray.setTitle("Unsuccessful");
+            tray.setMessage("Email must contain Emailname@domain.extension ");
+            tray.setNotificationType(NotificationType.ERROR);
+            tray.showAndDismiss(Duration.seconds(5));
+            throw new IllegalArgumentException();
+        }
+
+        if(Q1.compareTo(Q2) == 0 || Q2.compareTo(Q1) == 0)
+        {
+            tray.setTitle("Unsuccessful");
+            tray.setMessage("Security Questions Cannot be the same");
+            tray.setNotificationType(NotificationType.ERROR);
+            tray.showAndDismiss(Duration.seconds(5));
+            throw new IllegalArgumentException();
+        }
+
+        if (!Q1_Answer.matches(Answer_Format))
+        {
+            tray.setTitle("Unsuccessful Format");
+            tray.setMessage("A-Z and Numbers in Answer Field 1");
+            tray.setNotificationType(NotificationType.ERROR);
+            tray.showAndDismiss(Duration.seconds(5));
+            throw new IllegalArgumentException();
+        }
+
+        if (!Q2_Answer.matches(Answer_Format))
+        {
+            tray.setTitle("Unsuccessful Format");
+            tray.setMessage("A-Z and Numbers in Answer Field 2");
+            tray.setNotificationType(NotificationType.ERROR);
+            tray.showAndDismiss(Duration.seconds(5));
+            throw new IllegalArgumentException();
+        }
+
+    }
 }
